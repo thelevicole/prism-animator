556f0e3b210be703f7583c5e2738ee9a3f3e6f28
Levi Cole <levi@skape.co>
2022-03-01 14:40:19 +0000

    V2

--- README.md
+++ README.md
@@ -1,3 +1,4 @@
+
 # PrismJS Animator
 
 Simple code typing/animation library for [Prism](https://prismjs.com/) syntax highlighter.
@@ -12,8 +13,12 @@ Simple code typing/animation library for [Prism](https://prismjs.com/) syntax hi
 
 **JavasScript**
 ```javascript
-const editor = document.getElementById('editor');
-new PrismAnimator(editor, 'javascript', 'var example = "Hello world";', 600);
+new PrismAnimator({
+    element: document.getElementById('editor'),
+    language: 'javascript',
+    code: 'var example = "Hello world";',
+    charactersPerMinute: 600
+});
 ```
 
 
@@ -22,6 +27,17 @@ new PrismAnimator(editor, 'javascript', 'var example = "Hello world";', 600);
 $('#editor').prismAnimator({
     language: 'javascript',
     code: 'var example = "Hello world";',
-    cpm: 600
+    charactersPerMinute: 600
 });
 ```
+
+## All parameters
+| Parameter | Type | Description | Default | Required |
+|--|--|--|--|--|
+| `element` | `HTMLElement` | The target element to print the code. | `null` | `true` |
+| `language` | `String` | The name of the language definition. | `null` | `true` |
+| `code` | `String` | A string with the code to be highlighted. | `null` | `true` |
+| `charactersPerMinute` | `Number` | The number of characters to print per minute. | `200` | `false` |
+| `preClasses` | `String|Array` | Classes to add to the generated `<pre>` element. | `null` | `false` |
+| `codeClasses` | `String|Array` | Classes to add to the generated `<code>` element. | `null` | `false` |
+| `drawCallback` | `Function` | Callback function called everytime a new draw is made. | `null` | `false` |
--- src/PrismAnimator.js
+++ src/PrismAnimator.js
@@ -1,6 +1,13 @@
 class PrismAnimator {
 
     /**
+     * User defined options.
+     *
+     * @type {object}
+     */
+    options = {};
+
+    /**
      * @type {object}
      */
     elements = {};
@@ -46,30 +53,35 @@ class PrismAnimator {
     _lastTimestamp = 0;
 
     /**
-     * @param {HTMLElement} targetElement
-     * @param {string} language
-     * @param {string} code
-     * @param {number} charactersPerMinute
+     * @param {object} options
      */
-    constructor( targetElement, language, code, charactersPerMinute = 200 ) {
+    constructor( options = {} ) {
 
-        this.language = language;
-        this.codeString = code;
-        this.codeStringlength = code.length;
+        this.options = options;
 
-        this.charactersPerMinute = charactersPerMinute;
+        this.elements.parent = this.getOption( 'element' );
+
+        this.language = this.getOption( 'language' );
+        this.codeString = this.getOption( 'code' );
+        this.codeStringlength = this.codeString.length;
+
+        this.charactersPerMinute = this.getOption( 'charactersPerMinute', 200 );
         this.fpsInterval = 1000 / ( this.charactersPerMinute / 60 );
 
         // Sanity checks.
         if ( !( 'Prism' in window ) ) {
             throw new Error( 'Prism library not loaded' );
+        } else if ( !this.elements.parent ) {
+            throw new Error( `PrismAnimator is missing a required option 'element'. Pass via the constructor object e.g. new PrismAnimator( { element: document.getElementById( 'example' ) } )` );
+        } else if ( !this.language ) {
+            throw new Error( `PrismAnimator is missing a required option 'language'. Pass via the constructor object e.g. new PrismAnimator( { language: 'javascript' } )` );
+        } else if ( !this.codeString ) {
+            throw new Error( `PrismAnimator is missing a required option 'code'. Pass via the constructor object e.g. new PrismAnimator( { code: 'var example = 123;' } )` );
         } else if ( !( this.language in Prism.languages ) ) {
             throw new Error( `Language "${this.language}" not supported.` );
         }
 
         // Build DOM structure on init.
-        this.elements.parent = targetElement;
-
         this.elements.code = document.createElement( 'code' );
         this.elements.code.classList.add( `language-${this.language}` );
 
@@ -79,8 +91,30 @@ class PrismAnimator {
 
         this.elements.parent.append( this.elements.pre );
 
+        // Add user classes to `<pre>` element
+        const preClasses = this.getOption( 'preClasses', [] );
+
+        if ( Array.isArray( preClasses ) ) {
+            this.elements.pre.classList.add( ...preClasses );
+        } else if ( typeof preClasses === 'string' ) {
+            this.elements.pre.classList.add( preClasses );
+        }
+
+        // Add user classes to `<code>` element
+        const codeClasses = this.getOption( 'codeClasses', [] );
+
+        if ( Array.isArray( codeClasses ) ) {
+            this.elements.code.classList.add( ...codeClasses );
+        } else if ( typeof codeClasses === 'string' ) {
+            this.elements.code.classList.add( codeClasses );
+        }
+
         // Begin animation.
         requestAnimationFrame( this.update.bind( this ) );
+    }
+
+    getOption( key, defaultValue ) {
+        return this.options[ key ] || defaultValue;
     }
 
     update( timestamp = 0 ) {
@@ -96,8 +130,13 @@ class PrismAnimator {
         if ( shouldDrawFrame ) {
             this._lastTimestamp = timestamp - ( elapsed % this.fpsInterval );
             this.currentCharacterIndex++;
-            const part = this.codeString.substring( 0, this.currentCharacterIndex );
-            this.elements.code.innerHTML = Prism.highlight( part, Prism.languages[ this.language ], this.language );
+            this.elements.code.innerHTML = this.codeString.substring( 0, this.currentCharacterIndex );;
+
+            Prism.highlightElement( this.elements.code );
+
+            if ( this.getOption( 'drawCallback' ) ) {
+                this.getOption( 'drawCallback' ).apply( this );
+            }
         }
 
         if ( this.currentCharacterIndex < this.codeStringlength ) {
@@ -111,14 +150,23 @@ window.PrismAnimator = PrismAnimator;
 
 if ( window.jQuery ) {
     jQuery.fn.prismAnimator = function( options = {} ) {
-        
+
         options = $.extend( true, {
             language: 'javascript',
             code: 'var example = 123;',
-            cpm: 200
+            charactersPerMinute: 200,
+            preClasses: '',
+            codeClasses: '',
         }, options );
 
-        this._prismAnimator = new PrismAnimator( this.get( 0 ), options.language, options.code, options.cpm );
+        this._prismAnimator = new PrismAnimator( {
+            element: this.get( 0 ),
+            language: options.language,
+            code: options.code,
+            charactersPerMinute: options.charactersPerMinute,
+            preClasses: options.preClasses,
+            codeClasses: options.codeClasses
+        } );
 
         return this;
     };
